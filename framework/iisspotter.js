04f91968d6b81db546d0a82f6ca9e1312a0fbbc7
cbollerud <bollo@usa.com>
2011-06-24 22:09:54 -0700

    gps coordinates working

--- framework/iisspotter.js
+++ framework/iisspotter.js
@@ -1,5 +1,6 @@
 
 var phonegap = new PhoneGap();
+
 //must call this to let the device know that the app is ready
 navigator.device.deviceReady();
 
@@ -13,64 +14,6 @@ function info() {
 	lastpage.innerHTML = deviceinfo;
 }
 
-function net() {
-	page('net');
-	var netpage = lastpage;
-	netpage.innerHTML = "Requesting network info...";
-	
-	function successful(response) {
-		
-		var states = {};
-		        states[NetworkStatus.NOT_REACHABLE]                      = 'No network connection';
-		        states[NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK] = 'Carrier data connection';
-		        states[NetworkStatus.REACHABLE_VIA_WIFI_NETWORK]         = 'WiFi connection';
-		
-		
-		var r = JSON.stringify(states[response]);
-		netpage.innerHTML = r.replace(/,/g, ', ');
-	}
-	
-	var request = navigator.network.isReachable('palm://com.palm.connectionmanager', successful, {});
-}
-
-function displayCurrentOrientation() {
-	navigator.notification.showBanner(navigator.orientation.getCurrentOrientation(), 'current orientaiton'); 
-}	
-
-function getPicture() {
-	page('camera');
-	var cameraPage = lastpage;
-	lastpage.innerHTML = "camera application should launch if this works";
-	
-	function onSuccessCallback(response) {
-		console.log("camera launched successfully");
-		navigator.notification.showBanner("camera launched successfully");
-	}
-	
-	function onErrorCallback(response) {
-		console.log("camera failed to launch");
-		navigator.notification.showBanner("camera failed to launch");
-	}
-	
-	navigator.camera.getPicture(onSuccessCallback, onErrorCallback);
-}
-
-window.addEventListener("palmsystem", function(e) {
-	console.log("system message: " + JSON.stringify(e.data));
-});
-
-window.addEventListener("appmenuopen", function() {
-	menu(true);
-});
-
-window.addEventListener("back", function() {
-	console.log("back gesture");
-});
-
-window.addEventListener("forward", function() {
-	console.log("forward gesture");
-});
-
 function menu() {
 	var menudiv = document.getElementById('menu');
 	if (typeof state == 'undefined')
@@ -80,16 +23,46 @@ function menu() {
 }
 
 lastreq = null;
-function launchmap() {
-	navigator.map.show();
-	menu(false);
-}
 
 var lastpage = document.getElementById('screen');
+
 function page(id) {
 	lastpage.style.display = 'none';
 	lastpage = document.getElementById(id);
 	lastpage.style.display = 'block';
 	menu(false);
 }
+
+function onLoad() {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + 1234              + '<br />' +
+                            'Longitude: '          + 5678             + '<br />';
+}
+
+function onError(error) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'error getting location.';
+}
+
+function changeme() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          + new Date(position.timestamp)          + '<br />';
+}
+
+// PhoneGap is ready
+function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
 
